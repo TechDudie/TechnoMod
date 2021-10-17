59610304eb049681b36ba254f4d2778c259d6851
TechDudie <73961295+TechDudie@users.noreply.github.com>
2021-10-16 22:54:16 -0500

    Update ModItems.java

--- src/main/java/com.technodot.technomod/ModItems.java
+++ src/main/java/com.technodot.technomod/ModItems.java
@@ -10,7 +10,7 @@ import net.minecraftforge.registries.ForgeRegistries;
 public class ModItems {
 
     //The ITEMS deferred register in which you can register items.
-    public static final DeferredRegister<Item> ITEMS = new DeferredRegister<>(ForgeRegistries.ITEMS, TutorialMod.MOD_ID);
+    public static final DeferredRegister<Item> ITEMS = new DeferredRegister<>(ForgeRegistries.ITEMS, TechnoMod.MOD_ID);
 
     //Register the tutorial dust with "tutorial_dust" as registry name and default properties
     public static final RegistryObject<Item> TUTORIAL_DUST = ITEMS.register("tutorial_dust", () -> new Item(new Item.Properties()));
